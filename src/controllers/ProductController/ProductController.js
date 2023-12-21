bf2fe10f5d9c06e7e110e0d3415d914ae23dae1e
ricardoggz <ricardo_ggzicm@outlook.com>
2023-12-21 13:25:22 -0600

    se creo el arreglo para imagenes relacionadas con producto

--- src/controllers/ProductController/ProductController.js
+++ src/controllers/ProductController/ProductController.js
@@ -4,7 +4,26 @@ export function getProducts(req, res){
     const db = new DatabaseModel()
     db.pool.getConnection((err, conn)=>{
         if(err) return res.json(err)
-        conn.query('SELECT * FROM products', (err, rows)=>{
+        conn.query(
+        `SELECT
+        products.product_id,
+        products.product_name,
+        products.product_price,
+        products.product_color,
+        products.product_image,
+        products.category_id,
+        products.model_id,
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+            'imagen_id', imagenes.image_id,
+            'imagen_ruta', imagenes.image_name
+            )
+        ) as images
+        FROM
+        products
+        LEFT JOIN
+        imagenes ON imagenes.product_id = products.product_id;`,
+        (err, rows)=>{
             if(err) return res.json(err)
             return res.json(rows)
         })
